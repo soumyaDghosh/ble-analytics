b4d20e00ac2bb962c8cd4338aca5765b8b9830a9
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2026-07-15 01:49:33 +0530

    chore(db): migrate to postgres
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,10 +65,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db('DATABASE_URL', default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
